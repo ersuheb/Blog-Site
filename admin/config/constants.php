0703c7603c9cc7faf00dd364f8e782b0dc7f25a1
ersuheb <ersuheb@gmail.com>
2023-06-21 09:53:58 +0530

    updated

---
+++ admin/config/constants.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+define('ROOT_URL', 'http://localhost:8888/blog/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'suheb');
+define('DB_PW', '123456');
+define('DB_NAME', 'blog');
